c914d87aa7978fdf313734426c9c9ca9bcafd8df
MarkRegalla27 <MarkRegalla27@users.noreply.github.com>
2015-09-02 23:05:41 -0400

    Update q8_parsing.py

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -6,12 +6,39 @@
 
 
 import csv
+data = '/users/markregalla/desktop/metis/dsp_my_work_as_of_9-1-15/python/football.csv'
 
   def read_data(data):
-   # COMPLETE THIS FUNCTION
+    with open(data, 'rb') as f:
+      reader = csv.reader(f)
+      datalist = list(reader)
+      return datalist
+  
+  datalist = read_data(data)
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
+  def get_min_score_difference(datalist):
+    i = 1
+      difflist = []
+      while i < len(datalist):
+        sublist = datalist[i]
+        difflist.append(abs(int(sublist[6]) - int(sublist[5])))
+        i += 1
+      return difflist,  min(difflist)
+  
+  difflist, minDiff = get_min_score(datalist)
+  print 'The minimum difference between Goals and Goals Allowed is ' + str(minDiff)
+  
+  def get_team(difflist):
+    i = 0
+        k = 1
+        while i < len(difflist) - 1:
+                if difflist[i] > difflist[i + 1]:
+                        k += 1
+                i += 1
+        return k + 1
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+  indexOfTeam = get_team(difflist)
+  teamList = []
+  teamList = datalist[indexOfTeam]
+  team = teamList[0]
+  print 'The team with the lowest difference is ' + team
