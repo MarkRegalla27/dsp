335d5ef1f6004d63ea75c61a9856fe879d2e4f24
MarkRegalla27 <MarkRegalla27@users.noreply.github.com>
2015-09-06 17:54:40 -0400

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,7 @@
-PLACE YOUR CODE HERE
+import csv
+import re
+data = '/users/markregalla/desktop/metis/dsp_my_work_as_of_9-1-15/python/faculty.csv'
+
+with open(data, 'rb') as f:
+        reader = csv.reader(f)
+        datalist = list(reader)
