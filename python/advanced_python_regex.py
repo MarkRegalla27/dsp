67380af1bf8cfdad81adcf19c6f82a685aedd933
MarkRegalla27 <MarkRegalla27@users.noreply.github.com>
2015-09-06 17:58:37 -0400

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -5,3 +5,26 @@ data = '/users/markregalla/desktop/metis/dsp_my_work_as_of_9-1-15/python/faculty
 with open(data, 'rb') as f:
         reader = csv.reader(f)
         datalist = list(reader)
+        
+#Format for list order: name, degree, title, email
+i = 1
+namelist = []
+deglist = []
+titlelist = []
+emaillist = []
+while i < len(datalist):
+        sublist = datalist[i]
+        namelist.append(sublist[0])
+        deglist.append(sublist[1])
+        titlelist.append(sublist[2])
+        emaillist.append(sublist[3])
+        i += 1
+
+#Q1: Find number of different degrees:
+        
+#Q2: Find number of different titles:
+        
+#Q3: Print list of email addresses:
+print emaillist
+
+#Q4:
